9076eb1595c9f60585320f724961d62758befd97
davecash75 <d.cash@ucl.ac.uk>
2022-04-28 13:51:42 +0100

    Moving more helper functions over.

--- 3_UsingTheEventBasedModel/kde_utils.py
+++ 3_UsingTheEventBasedModel/kde_utils.py
@@ -1,4 +1,7 @@
 import itertools
+import numpy as np
+from kde_ebm.mixture_model import get_prob_mat
+
 def extract_pvd(ml_order,samples):
     if type(ml_order) is list:
         #* List of PVDs from cross-validation/bootstrapping
@@ -71,3 +74,62 @@ def save_plot(fig, fname, fig_format="png", dpi=150, **kwargs):
         bbox_inches="tight",
         **kwargs
     )
+    return
+
+
+
+    #* Define the EBM staging function
+def ebm_staging(x,mixtures,samples):
+    """
+    Given a trained EBM (mixture_models,mcmc_samples), and correctly-formatted data, stage the data
+    NOTE: To use CV-EBMs, you'll need to call this for each fold, then combine.
+    Author: Neil P Oxtoby, UCL, September 2018
+    """
+    if type(mixtures[0]) is list:
+        #* List of mixture models from cross-validation / bootstrapping
+        n_cv = len(mixtures)
+        prob_mat = []
+        stages = []
+        stage_likelihoods = []
+        stages_expected = []
+        for k in range(n_cv):
+            #* Stage the data
+            prob_mat.append(get_prob_mat(x, mixtures[k]))
+            stages_k, stage_likelihoods_k = samples[k][0].stage_data(prob_mat[k])
+            stages.append(stages_k)
+            stage_likelihoods.append(stage_likelihoods_k)
+            #* Average (expectation value) stage
+            stages_expected_k = np.ndarray(shape=stages_k.shape)
+            for kk in range(stages_expected_k.shape[0]):
+                stages_expected_k[kk] = np.sum(stage_likelihoods_k[kk,:]*np.arange(1,stage_likelihoods_k.shape[1]+1,1))/np.sum(stage_likelihoods_k[kk,:]) - 1
+            stages_expected.append(stages_expected_k)
+    else:
+        #* Stage the data
+        prob_mat = get_prob_mat(x, mixtures)
+        if type(samples[0]) is list:
+            n_bs = len(samples)
+            stages = []
+            stage_likelihoods = []
+            stages_expected = []
+            for k in range(n_bs):
+                #* Stage the data
+                stages_k, stage_likelihoods_k = samples[k][0].stage_data(prob_mat)
+                stages.append(stages_k)
+                stage_likelihoods.append(stage_likelihoods_k)
+                #* Average (expectation value) stage
+                stages_expected_k = np.ndarray(shape=stages_k.shape)
+                for kk in range(stages_expected_k.shape[0]):
+                    stages_expected_k[kk] = np.sum(stage_likelihoods_k[kk,:]*np.arange(1,stage_likelihoods_k.shape[1]+1,1))/np.sum(stage_likelihoods_k[kk,:]) - 1
+                stages_expected.append(stages_expected_k)
+        else:
+            stages, stage_likelihoods = samples[0].stage_data(prob_mat)
+            #* Average (expectation value) stage
+            stages_expected = np.ndarray(shape=stages.shape)
+            for k in range(stages_expected.shape[0]):
+                stages_expected[k] = np.sum(stage_likelihoods[k,:]*np.arange(1,stage_likelihoods.shape[1]+1,1))/np.sum(stage_likelihoods[k,:]) - 1
+    # #* Average (expectation value) stage
+    # stages_expected_n = np.sum(stage_likelihoods,axis=1)
+    # stages_expected_ = np.average(stage_likelihoods_long_ml,axis=1,weights=np.arange(1,stage_likelihoods_long_ml.shape[1]+1,1))
+    # stages_expected_ = stages_expected_/stages_expected_n
+    
+    return prob_mat, stages, stage_likelihoods, stages_expected
